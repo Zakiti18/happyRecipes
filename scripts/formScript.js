484b60cf14ce1c0bdd5f28fcd6a7cb23f433cbba
Cesar Escalona <cesarescalona10@gmail.com>
2021-06-02 15:49:29 -0400

    Fixed validation issues on the form, made sure to include checking for fields if switch is checked.

--- scripts/formScript.js
+++ scripts/formScript.js
@@ -2,17 +2,18 @@
   This is the script file for the form.html page
   Authors: Phillip Ball, Cesar Escalona
   File: form_script.js
-  Date: 06/01/2021
+  Date: 06/02/2021
 */
 
 document.getElementById("weekly").onsubmit = validate;
 
 function validate()
 {
+    // initialize a flag variable to check the form validation
     let Valid = true;
 
     //clear all error msgs
-    let errors = document.getElementsByClassName("error")
+    let errors = document.getElementsByClassName("err")
     for (let i = 0; i < errors.length; i++) {
         errors[i].style.display = "none";
     }
@@ -48,6 +49,52 @@ function validate()
         errPhone.style.display = "inline";
         Valid = false;
     }
-    console.log(Valid);
+
+    // Check if checkbox is selected, if so, require rest of validation
+    let checkBox = document.getElementById("switch");
+    if(checkBox.checked === true) {
+
+        // Check address
+        let address = document.getElementById("address").value;
+        if(address === ""){
+            let errAddress = document.getElementById("err-address");
+            errAddress.style.display = "inline";
+            Valid = false;
+        }
+
+        // Check city
+        let city = document.getElementById("city").value;
+        if(city === ""){
+            let errCity = document.getElementById("err-city");
+            errCity.style.display = "inline";
+            Valid = false;
+        }
+
+        // Check state
+        let state = document.getElementById("state").value;
+        if(state === ""){
+            let errState = document.getElementById("err-state");
+            errState.style.display = "inline";
+            Valid = false;
+        }
+
+        // Check zip
+        let zip = document.getElementById("zip").value;
+        if(zip === ""){
+            let errZip = document.getElementById("err-zip");
+            errZip.style.display = "inline";
+            Valid = false;
+        }
+
+        // Check country
+        let country = document.getElementById("country").value;
+        if(country === ""){
+            let errCountry = document.getElementById("err-country");
+            errCountry.style.display = "inline";
+            Valid = false;
+        }
+    }
+
+    // return if the form is valid or not
     return Valid;
 }
